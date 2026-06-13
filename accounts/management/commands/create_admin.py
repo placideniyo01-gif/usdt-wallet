320eb02011a2354c05b15f1dc5ff47a28b8877d3
placideniyo01-gif <placideniyo01@gmail.com>
2026-06-14 00:20:09 +0200

    Add create_admin command

---
+++ accounts/management/commands/create_admin.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Command(BaseCommand):
+    def handle(self, *args, **kwargs):
+
+        if not User.objects.filter(username="admin").exists():
+
+            User.objects.create_superuser(
+                username="admin",
+                email="admin@gmail.com",
+                password="Admin12345"
+            )
+
+            self.stdout.write("Superuser created")
+
+        else:
+            self.stdout.write("Superuser already exists")
