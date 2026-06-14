4a1d71b5ec7642d0c045e611e010c1029221f6f8
placideniyo01-gif <placideniyo01@gmail.com>
2026-06-14 14:44:05 +0200

    Add Cloudinary media storage

--- config/settings.py
+++ config/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'wallet_app',
     'transactions',
     'core',
+    'cloudinary',
+    'cloudinary_storage',
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -127,6 +129,16 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = (
+    'cloudinary_storage.storage.MediaCloudinaryStorage'
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
